4fe468b16e16e5734716df2d0c31f8145949db09
Jeanette <khojeanette@gmail.com>
2024-02-10 21:18:37 +0000

    Updated files with Jen's exercise 12, Q1-Q3 code. Added lottery_history.txt and initial gitignore file to hide idea folder

--- ana_ex12_Q1.py
+++ ana_ex12_Q1.py
@@ -24,3 +24,18 @@ cheese.insert(0, 'cottage-cheese')
 print(f"{'':<0} {cheese}")
 cheese.sort()
 print(f"{'':<1}Sorted cheese: {cheese}")
+
+# Created a list with 3 items and stored in it in the cheese variable
+cheese = ['Cheddar', 'Stilton', 'Cornish Yarg']
+
+# Prints a string announcing question 1 with new lines before and after
+print('\n', '-' * 100, '\nJen - QUESTION 1\n')
+
+# Adding Oke in this way adds each character into the list
+# cheese += 'Oke'
+
+# Square brackets MUST be included to add the whole word as one item in the list
+cheese += ['Oke', 'Mozarella']
+
+# Prints the new list with the additional two items
+print('Added two items to cheese list:\n', cheese)
