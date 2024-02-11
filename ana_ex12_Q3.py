e38b4da4edab3a7c448ce418e75ff93e687a8d8f
Anagha <mailsforanagha@gmail.com>
2024-02-11 22:01:39 +0000

    del branch ana

--- ana_ex12_Q3.py
+++ ana_ex12_Q3.py
@@ -13,7 +13,7 @@ full = False
 while not full:
     # add method is used to add elements to set, each time a random integer
     lotto_set.add(random.randint(1, 50))
-    # if loop checks the condition if set has been filled with 6 numbers and accordingly
+    # if condition checks whether set has been filled with 6 numbers and accordingly
     # changes the value of boolean variable full
     if len(lotto_set) == 6:
         full = True
