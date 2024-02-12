5f3cc48b9a8b3a97f9777ca54c6186f928f47b92
Anagha <mailsforanagha@gmail.com>
2024-02-12 16:42:54 +0000

    Added while loop as alternative in read_file and written another file for practice in write_file.

--- ana_ex13_read_file.py
+++ ana_ex13_read_file.py
@@ -13,7 +13,7 @@ file.close()
 
 # creating an empty list
 lines_list = []
-# for loop iterates until there are lines to read in file
+# for loop iterates until there are lines to read in file and closes the file in the end
 for line in open("pelican.txt", 'r'):
     # appends line along with \n
     # lines_list.append(line)
@@ -22,8 +22,35 @@ for line in open("pelican.txt", 'r'):
     # strip() method to remove the end of line character
     lines_list.append(line.strip())
 # print the type of lines_list
+# no need to close the file as it gets closed at the end of for loop
 print("\n", type(lines_list))
 # prints the lines_list
-print(lines_list)
-# closes the file
+print(lines_list, "\n")
+
+# Opening another file
+file = open('nests.txt', 'r+')
+# slurps the limerick1.txt file in one go - stores the entire content in string variable limerick1
+limerick1 = file.read()
+# print type and value of limerick1
+print(type(limerick1), "\n", limerick1)
+
+# re-doing the iterative read using while loop instead of for
+# initialize empty list
+lines_list2 = []
+# since the file cursor is currently at the end of file, we are seeking the start of file.
+file.seek(0)
+# while loop runs until break command is executed
+while True:
+    # line variable is assigned the string value returned by readline() after stripping the newline character
+    line = file.readline()
+    # checks if there is no line left to read in the file
+    if not line:
+        # breaks the loop and goes to code after loop outside
+        break
+    # appends line to lines_list2
+    lines_list2.append(line)
+# printing the type of lines_list2 followed by its value
+print("\n", type(lines_list2))
+print(lines_list2)
+# close the file nests.txt
 file.close()
--- ana_ex13_write_file.py
+++ ana_ex13_write_file.py
@@ -12,5 +12,18 @@ lines = ["He can take in his beak,\n", "Enough for a week,\n",
          "But I am damned if I see how the helican."]
 # writing list of lines into the file
 file.writelines(lines)
+# close the pelican.txt file
+file.close()
 # pelican.txt got created on running the above code.
 # \n is useful and the file pelican.txt holds a set of lines one below the other
+
+
+# Assign string value to limerick1_str
+limerick1_str = ('There was an Old Man with a beard,\nWho said, "It is just as I feared! -\nTwo Owls and a Hen, '
+                 '\nFour larks and a Wren, \nHave all built their nests in my beard!"')
+# File is created or opened in updating mode w+ if it already exists
+file = open("nests.txt", 'w+')
+# writing string limerick1_str  into the file
+file.writelines(limerick1_str)
+# close the nests.txt file
+file.close()
